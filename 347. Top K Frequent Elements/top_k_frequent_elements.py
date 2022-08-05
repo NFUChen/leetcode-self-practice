898e9828bea3cb90badfb0405c14562f4f021fe1
William Chen <ow6201231@gmail.com>
2022-08-05 22:50:17 +0800

    modify topK frequent elements

--- 347. Top K Frequent Elements/top_k_frequent_elements.py
+++ 347. Top K Frequent Elements/top_k_frequent_elements.py
@@ -10,17 +10,18 @@ class Solution:
             return count_map
         
         counts_arr = [
-            [] for _ in range(len(nums))
+            [] for _ in range(len(nums) + 1)
         ]
         
         count_map = get_count_map(nums)
         
         for num, count in count_map.items():
-            counts_arr[count - 1].append(num)
+            counts_arr[count].append(num)
         
         top_k = []
         for idx in range(len(counts_arr) - 1, -1, -1):
-            while(counts_arr[idx] != []):
-                top_k.append(counts_arr[idx].pop())
+            for num in counts_arr[idx]:
+                top_k.append(num)
                 if len(top_k) == k:
                     return top_k
+                
