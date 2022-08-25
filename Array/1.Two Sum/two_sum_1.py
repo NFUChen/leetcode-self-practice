0fd596dc4af3e2e079664da597d458723518eb7d
William Chen <ow6201231@gmail.com>
2022-08-25 22:24:31 +0800

    remove files

--- Array/1.Two Sum/two_sum_1.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for idx in range(len(nums)):
-            current_num = nums[idx]
-            diff = target - current_num
-            seen[current_num] = idx
-            if diff in seen:
-                return [seen[diff], idx]
